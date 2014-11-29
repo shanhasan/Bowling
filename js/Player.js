91f753f52d0bd97fc2319bc8d134b2a5fe06984d
Giorgia <giorgia1london@gmail.com>
2014-11-29 01:50:38 +0100

    setup

---
+++ js/Player.js
@@ -0,0 +1,15 @@
+function Player(name) {
+	this.name = name;
+};
+
+Player.prototype.roll = function() {
+	this.roll = roll;
+};
+
+function Game(player) {
+	this.player1 = player1;
+};
+
+Game.prototype.winner = function(first_argument) {
+	// body...
+};
